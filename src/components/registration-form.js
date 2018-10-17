82d5819dea9dda3c1b536559db4c709aab3d0416
Timothy <superfluous11@gmail.com>
2018-10-17 15:15:01 -0700

    Updated the update claim form

--- src/components/registration-form.js
+++ src/components/registration-form.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { reduxForm, Field, SubmissionError } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { Field, SubmissionError } from 'redux-form';
 import { registerUser } from '../actions/users';
 import { login } from '../actions/auth';
 import Input from './input';
 import BaseForm from './form';
-import { Link, Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { required, trimmed, match } from '../validators';
 import './form.css';
+
 const matchPassword = match('password');
 const BaseRegistrationForm = BaseForm('registration');
 
@@ -22,14 +24,17 @@ class RegistrationForm extends React.Component {
   }
 
   render(){
-    const { loggedIn } = this.props;
-    if(loggedIn) return <Redirect to='/dashboard'/>;
+    if(this.props.loggedIn) return <Redirect to='/dashboard'/>;
+
     return(
-      <BaseRegistrationForm title='REGISTER' onSubmit={values => this.registerUser(values)}>
-        <Field component={Input} type='text' label='Username' name='username' validate={[required, trimmed]}/>
-        <Field component={Input} type='password' label='Password' name='password' validate={[required, trimmed]}/>          
-        <Field component={Input} type='password' label='Confirm password' name='passwordConfirm' validate={[required, matchPassword]}/>
-      </BaseRegistrationForm>
+      <div>
+        <BaseRegistrationForm title='REGISTER' onSubmit={values => this.registerUser(values)}>
+          <Field component={Input} type='text' label='Username' name='username' validate={[required, trimmed]}/>
+          <Field component={Input} type='password' label='Password' name='password' validate={[required, trimmed]}/>          
+          <Field component={Input} type='password' label='Confirm password' name='passwordConfirm' validate={[required, matchPassword]}/>
+        </BaseRegistrationForm>
+        <Link className='form-link' to='/login'>Login</Link>
+      </div>
     );
   }
 }
--- src/components/update-claim-form.js
+++ src/components/update-claim-form.js
@@ -1,55 +1,47 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { reset, reduxForm, Field } from 'redux-form';
+import { Field } from 'redux-form';
 import Input from './input';
-import { Link } from 'react-router-dom';
 import { fetchClaims, updateClaim } from '../actions/claims';
 import { required } from '../validators';
+import BaseForm from './form';
+
+const BaseUpdateClaimForm = BaseForm('updateClaim');
 
 class UpdateClaimForm extends React.Component{
 
   updateClaim(values){
     const { claimId, transactionDate, lossPayment, caseReserve, status } = values;
-
     const claim = this.props.claims.find(claim => claim.id === claimId);
     const transactions = claim.transactions.slice();
-    transactions.push({ 
+    transactions.push({
       transactionDate,
       lossPayment,
       caseReserve
     });
     const updatedClaim = {
-      ...claim,
-      status: status ? 'CLOSED': 'OPEN',
+      ...claim, 
+      status: status ? 'CLOSED' : 'OPEN',
       transactions
     };
-
     return this.props.dispatch(updateClaim(updatedClaim))
       .then(() => this.props.dispatch(fetchClaims()));
   }
 
   render(){
-
-    const { claims, handleSubmit, pristine, submitting, error } = this.props;
-    const options = claims.filter(claim => claim.status === 'OPEN').map(({ id }) => <option key={id} value={id}>{id}</option>);
-  
-    return(
-      <div>
-        <Link to='/dashboard'>{'< BACK'}</Link>
-        <h2>RECORD PAID LOSS OR CASE RESERVE ON CLAIM</h2> 
-        <form className='form' onSubmit={handleSubmit(values => this.updateClaim(values))}>
-          {error}
-          <Field component={Input} type='date' label='Transaction Date' name='transactionDate' validate={[required]}/>
-          <Field component={Input} element='select' label='Choose ID of an Existing Claim' name='claimId' validate={[required]}>
-            <option value=''></option>
-            {options}
-          </Field>
-          <Field component={Input} label='Record a Loss Payment' type='number' name='lossPayment' min='0' validate={[required]}/>
-          <Field component={Input} label='Update the Case Reserve' type='number' name='caseReserve' min='0' validate={[required]}/>
-          <Field component={Input} className='checkbox' label='Close the Claim?' type='checkbox' name='status' min='0'/>
-          <button type='submit' disabled={pristine || submitting}>SUBMIT</button>
-        </form>
-      </div>
+    const options = this.props.claims.filter(claim => claim.status === 'OPEN').map(({ id }) => <option key={id} value={id}>{id}</option>);
+    
+    return (
+      <BaseUpdateClaimForm title='RECORD PAID LOSS OR CHANGE CASS RESERVE' onSubmit={values => this.updateClaim(values)}>
+        <Field component={Input} type='date' label='Transaction Date' name='transactionDate' validate={[required]}/>
+        <Field component={Input} element='select' label='Choose ID of an Existing Claim' name='claimId' validate={[required]}>
+          <option value=''></option>
+          {options}
+        </Field>
+        <Field component={Input} label='Record a Loss Payment' type='number' name='lossPayment' min='0' validate={[required]}/>
+        <Field component={Input} label='Update the Case Reserve' type='number' name='caseReserve' min='0' validate={[required]}/>
+        <Field component={Input} className='checkbox' label='Close the Claim?' type='checkbox' name='status' min='0'/>
+      </BaseUpdateClaimForm>
     );
   }
 }
@@ -57,10 +49,5 @@ class UpdateClaimForm extends React.Component{
 const mapStateToProps = state => ({
   claims: state.claims
 });
-const WrappedAddClaimForm = connect(mapStateToProps)(UpdateClaimForm);
 
-export default reduxForm({
-  form: 'updateclaim',
-  initialValues: { claimId: '' },
-  onSubmitSuccess: (result, dispatch) => dispatch(reset('updateclaim'))
-})(WrappedAddClaimForm);
+export default connect(mapStateToProps)(UpdateClaimForm);
