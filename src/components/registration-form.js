ebe392039843a94d502c27370130ccc038fbe5de
Timothy <superfluous11@gmail.com>
2018-10-16 21:02:50 -0700

    Updated registration form to automatically login after registration

--- src/components/registration-form.js
+++ src/components/registration-form.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { reduxForm, Field } from 'redux-form';
 import { registerUser } from '../actions/users';
 import { login } from '../actions/auth';
 import Input from './input';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { required, trimmed, match } from '../validators';
 import './form.css';
 const matchPassword = match('password');
@@ -18,15 +19,18 @@ class RegistrationForm extends React.Component{
   }
 
   render(){
+    const { handleSubmit, pristine, submitting, loggedIn } = this.props;
+    if(loggedIn) return <Redirect to='/dashboard'/>;
+    
     return (
       <div>
         <h2>REGISTER</h2>
-        <form onSubmit={this.props.handleSubmit(values => this.registerUser(values))}>
+        <form onSubmit={handleSubmit(values => this.registerUser(values))}>
           <div class='form'>
-            <Field component={Input} type='text' label='Username' name='username' id='username' validate={[required, trimmed]}/>
-            <Field component={Input} type='password' label='Password' name='password' id='password' validate={[required, trimmed]}/>          
-            <Field component={Input} type='password' label='Confirm password' name='passwordConfirm' id='passwordConfirm' validate={[required, matchPassword]}/>
-            <button type='submit' disabled={this.props.pristine||this.props.submitting}>SUBMIT</button>
+            <Field component={Input} type='text' label='Username' name='username' validate={[required, trimmed]}/>
+            <Field component={Input} type='password' label='Password' name='password' validate={[required, trimmed]}/>          
+            <Field component={Input} type='password' label='Confirm password' name='passwordConfirm' validate={[required, matchPassword]}/>
+            <button type='submit' disabled={pristine||submitting}>SUBMIT</button>
             <Link to='/login'>Login</Link>
           </div>
         </form>
@@ -35,6 +39,10 @@ class RegistrationForm extends React.Component{
   }
 }
 
+const mapStateToProps = state => ({
+  loggedIn: state.auth.currentUser !== null
+});
+
 export default reduxForm({
   form: 'registration'
-})(RegistrationForm);
+})(connect(mapStateToProps)(RegistrationForm));
